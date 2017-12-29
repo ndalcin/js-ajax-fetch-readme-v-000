bd3cd5c298cdd55efa71e0cce1b10daec0998984
ndalcin <dalcin.nicole@gmail.com>
2017-12-29 07:39:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
